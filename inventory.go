bb5a114bf5b2f4596677185ec80f48d4c05722ef
Grant Regimbal <grant.r@pricewaiter.com>
2015-08-02 18:02:50 -0700

    go fmt

--- inventory.go
+++ inventory.go
@@ -4,29 +4,29 @@ import "fmt"
 import "os"
 
 type item struct {
-	id int
-	name string
+	id          int
+	name        string
 	description string
-	quantity int
-	location string
+	quantity    int
+	location    string
 }
 
 func main() {
-		args := os.Args[1]
+	args := os.Args[1]
 
-		switch args {
-		case "list":
-				fmt.Println("List current args")
-		case "create":
-				fmt.Println("create a new item")
-		case "read":
-				fmt.Println("look at a specific item")
-		case "update":
-				fmt.Println("change an item")
-		case "delete":
-				fmt.Println("delete an item")
-		default:
-				fmt.Println("Options include list, create, read, update, delete")
-		}
+	switch args {
+	case "list":
+		fmt.Println("List current args")
+	case "create":
+		fmt.Println("create a new item")
+	case "read":
+		fmt.Println("look at a specific item")
+	case "update":
+		fmt.Println("change an item")
+	case "delete":
+		fmt.Println("delete an item")
+	default:
+		fmt.Println("Options include list, create, read, update, delete")
+	}
 
 }
