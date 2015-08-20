c0c47d7af65c0b55dd7de30ea5b2ccc0d7b95587
Grant Regimbal <regimbal.grant@gmail.com>
2015-08-19 17:57:12 -0700

    inventory logic

--- inventory.py
+++ inventory.py
@@ -18,8 +18,30 @@ class Item(Model):
         database = db
 
 
+
+def str_to_usd(string):
+     amount = int(string)
+     return Money(amount=amount,currency='USD')
+
 def init():
-	db.connect()
-	db.create_tables([Item])
+    db.connect()
+    db.create_tables([Item])
 
+def value():
+    for item in Item.select():
+	print item.value
 
+def list():
+    print "name | quantity | value"
+    for item in Item.select():
+	print str(item.name) + "|" + str(item.quantity)  + "|" + str(item.value)
+
+def add():
+    item = Item()
+    item.name = raw_input("name: ")
+    item.value = Money(amount=raw_input("amount: "), currency='USD')
+    item.location = raw_input("location: ")
+    item.quantity = int(raw_input("quantity: "))
+    item.notes = raw_input("notes: ")
+    item.date = date.today()
+    item.save()
